a13fbd00cf615db3acde7e31c1cffd54da164ead
Milton Fernado Molloja <milton.molloja@gmail.com>
2019-03-17 16:37:00 -0300

    Categoria MVC - Defaul

---
+++ Sistema/Sistema/Data/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sistema.Models;
+
+namespace Sistema.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SistemaContext context)
+        {
+            context.Database.EnsureCreated();
+            //buscar si exiten Registros en la Tabla Categoria
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+            var categorias = new Categoria[]
+            { new Categoria{ Nombre = "Programacion", Descripcion="Curso de Programacion", Estado=true},
+            new Categoria{ Nombre = "Diseño", Descripcion="Curso de Diseño Grafico", Estado=true},
+            new Categoria{ Nombre = "Idioma", Descripcion="Curso de Ingles", Estado=true}
+            };
+
+            foreach (Categoria c in categorias)
+            {
+                context.Categoria.Add(c);
+
+            }
+            context.SaveChanges();
+        }
+    }
+}
