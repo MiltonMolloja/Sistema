2b412b247082410623f722896a75ab358a2aa9f4
Milton Fernado Molloja <milton.molloja@gmail.com>
2019-03-17 15:22:52 -0300

    Vistas VCM

---
+++ Sistema/Sistema/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        [Required(ErrorMessage = "Campo Requerido - Nombre")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Ingrese Entre 3 a 50 Caracteres")]
+        public string Nombre { get; set; }
+        [StringLength(250)]
+        [Display (Name ="Descripción") ]
+        public string Descripcion { get; set; }
+        public Boolean Estado { get; set; }
+    }
+}
